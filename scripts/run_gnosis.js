864e49202303fd8e0423e8a340e1f6ec46cd1301
Collin Chin <collin.chin@berkeley.edu>
2018-05-26 17:24:34 -0300

    market deploy script working

--- scripts/run_gnosis.js
+++ scripts/run_gnosis.js
@@ -1,131 +1,103 @@
-import Gnosis from '@gnosis.pm/pm-js'
+var Gnosis = require('@gnosis.pm/pm-js')
+// var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+var HDWalletProvider = require("truffle-hdwallet-provider")
+var config = require('./config.json')
 
-let gnosis
-let ipfsHash
-let oracle
-let event
-let market
-let localCalculatedCost
-let localCalculatedProfit
+var gnosis
+var ipfsHash
+var oracle
+var event
+var market
+var localCalculatedCost
+var localCalculatedProfit
 
-/* 
- * STEP 1 CONNECT TO GNOSIS 
- */
-export const initGnosisConnnection = async (GNOSIS_OPTIONS) => {
-    try {
-        gnosis = await Gnosis.create(GNOSIS_OPTIONS)   
-        console.info("Connected to Gnosis")
-    } catch(err) {
-        console.error("unable to connect to Gnosis :(")
-        console.error(err)
+const LOWERBOUND = config.LOWERBOUND
+const UPPERBOUND = config.UPPERBOUND
+
+const provider = new HDWalletProvider("stool arrow fatigue sunny actual bind radio license enemy peanut penalty soccer", "https://rinkeby.infura.io/O6AgF19kplltX0VsPbYg");
+
+const GNOSIS_OPTIONS = {
+    ethereum: provider,
+    ipfs: config.ipfs,
+    gnosisdb: config.gnosisdb,
+    defaultAccount: config.defaultAccount
+}
+
+const GNOSIS_DESCRIPTION = {
+    title: config.GNOSIS_DESCRIPTION.title,
+    description: config.GNOSIS_DESCRIPTION.description,
+    resolutionDate: new Date(config.GNOSIS_DESCRIPTION.resolutionDate).toISOString(),
+    LOWERBOUND,
+    UPPERBOUND,
+    decimals: config.GNOSIS_DESCRIPTION.decimals,
+    unit: config.GNOSIS_DESCRIPTION.unit
+}
+
+async function runGnosis() {
+    switch(config.resolveMarketFlag) {
+        case 0:
+            console.log("creating market")
+
+            /* 
+             * STEP 1 CONNECT TO GNOSIS AND ADD ACCOUNT
+             */
+            try {
+                gnosis = await Gnosis.create(GNOSIS_OPTIONS)   
+                console.info("Connected to Gnosis")
+            } catch(err) {
+                console.error("unable to connect to Gnosis :(")
+                console.error(err)
+            }
+
+            /* 
+             * STEP 2 DEFINE EVENT AND UPLOAD TO IPFS
+             */
+            ipfsHash = await gnosis.publishEventDescription(GNOSIS_DESCRIPTION)
+            console.info(`Ipfs hash: https://ipfs.infura.io/api/v0/cat?stream-channels=true&arg=${ipfsHash}`)
+
+            /* 
+             * STEP 3 CREATE ORACLE FOR EVENT 
+             */
+            oracle = await gnosis.createCentralizedOracle(ipfsHash)
+            console.info(`Oracle created with address ${oracle.address}`)
+
+            /* 
+             * STEP 4 CREATE SCALAR EVENT TYPE
+             */
+            event = await gnosis.createScalarEvent(
+                gnosis.etherToken,
+                oracle,
+                LOWERBOUND,
+                UPPERBOUND
+            )
+            console.info(`Event created with address ${event.address}`)
+
+            /*
+             * STEP 5 CREATE AND FUND MARKET
+             */
+            market = await gnosis.createMarket({
+                marketFactory: gnosis.standardMarketFactory,
+                event: event,
+                marketMaker: gnosis.lmsrMarketMaker,
+                fee: 0, // 0%
+            })
+            console.info(`Market created with address ${market.address}`)
+            await gnosis.etherToken.deposit({value: config.FUNDING})
+            await gnosis.etherToken.approve(market.address, config.FUNDING) 
+            await market.fund(config.FUNDING)
+
+            break;
+        case 1:
+            console.log("closing market")
+            /* LAST STEP
+             * STEP 8 RESOLVE ORACLE
+             */
+            var oracleIsSet = await oracle.isSet()
+            if (oracleIsSet) {
+                await oracle.setOutcome(parseInt(config.EVENT_OUTCOME, 10))
+            }
+            break;
     }
 }
 
-/* 
- * STEP 2 DEFINE EVENT AND UPLOAD TO IPFS
- */
-export const defineEventDescription = async (GNOSIS_DESCRIPTION) => {
-    ipfsHash = await gnosis.publishEventDescription(GNOSIS_DESCRIPTION)
-    
-    console.info(`Ipfs hash: https://ipfs.infura.io/api/v0/cat?stream-channels=true&arg=${ipfsHash}`)
-}
-
-/* 
- * STEP 3 CREATE ORACLE FOR EVENT 
- */
-export const createOracle = async () => {
-    oracle = await gnosis.createCentralizedOracle(ipfsHash)
-
-    console.info(`Oracle created with address ${oracle.address}`)
-}
-
-/* 
- * STEP 4 CREATE SCALAR EVENT TYPE
- */
-export const createScalarEvent = async (LOWERBOUND, UPPERBOUND) => {
-
-    event = await gnosis.createScalarEvent({
-        collateralToken: gnosis.etherToken,
-        oracle,
-        // Note that these bounds should match the values published on IPFS
-        LOWERBOUND,
-        UPPERBOUND,
-    })
-
-    console.info(`Event created with address ${event.address}`)
-}
-
-/*
- * STEP 5 CREATE AND FUND MARKET
- */
-export const createMarketAndFund = async (FUNDING) => {
-    market = await gnosis.createMarket({
-        marketFactory: gnosis.standardMarketFactory,
-        event: event,
-        marketMaker: gnosis.lmsrMarketMaker,
-        fee: 0, // 0%
-    })
-
-    console.info(`Market create with address ${market.address}`)
-
-    await gnosis.etherToken.deposit({value: FUNDING})
-    await gnosis.etherToken.approve(market.address, FUNDING) 
-    await market.fund(FUNDING)
-}
-
-/*
- * STEP 6 COMPARE LMSR ESTIMATIONS
- * CHANGE VARIABLES BELOW BEFORE PUBLISH
- */
-let outcomeTokenIndex = 0 
-let outcomeTokenCount = 2.5e17
-let netOutcomeTokensSold = [0, 0]
-export const compareLMSR = async (FUNDING) => {
-    let lmsrData = {
-        netOutcomeTokensSold,
-        funding,
-        outcomeTokenIndex,
-        outcomeTokenCount,
-    }
-    localCalculatedCost = Gnosis.calcLMSRCost(lmsrData)
-    netOutcomeTokensSold[outcomeTokenIndex] += outcomeTokenCount
-    let numOutcomeTokensToSell = 2.5e17
-    localCalculatedProfit = Gnosis.calcLMSRProfit({      
-        netOutcomeTokensSold,      
-        funding,     
-        outcomeTokenIndex,      
-        outcomeTokenCount: numOutcomeTokensToSell,
-    })
-}
-
-/*
- * STEP 7 BUY AND SELL MARKET SHARES
- */
-export const buyShares = async () => {
-    await gnosis.etherToken.deposit(localCalculatedCost)
-    let acualCost = await gnosis.buyOutcomeTokens({       
-        market,       
-        outcomeTokenIndex,       
-        outcomeTokenCount   
-    })
-}
-
-export const sellShares = async () => {
-    let actualProfit = await gnosis.sellOutcomeTokens({      
-        market,       
-        outcomeTokenIndex,             
-        outcomeTokenCount: numOutcomeTokensToSell
-    }) 
-}
-
-/* LAST STEP
- * STEP 8 RESOLVE ORACLE
- */
-export const resolveOracle = async (EVENT_RESULT) => {
-    let oracleIsSet = await oracle.isSet()
-    if (oracleIsSet) {
-        await oracle.setOutcome(parseInt(EVENT_RESULT, 10))
-    }
-}
-
-export default Gnosis
+runGnosis()
