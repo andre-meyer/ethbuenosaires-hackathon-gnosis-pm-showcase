828edb5c5b6323ffd48d578bc9864cba2e173dd6
Collin Chin <collin.chin@berkeley.edu>
2018-05-25 23:03:14 -0300

    utilsjs added

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const description = {
+    title: 'How many projects will be shipped out of ETHBuenosAires',
+    description: 'A prediction market to determine the amount of completed projects that the 2018 ETHBuenosAires Hackathon will produce',
+    resolutionDate: new Date("2018-05-27T10:00:00Z").toISOString(),
+    lowerbound,
+    upperbound,
+    decimals: 0,
+    unit: "projects"
+}
