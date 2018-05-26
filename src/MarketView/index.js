96a14e0df35bdb7a95139ed95175e096e01c3222
Andre Meyer <andre.meyer93@gmail.com>
2018-05-25 22:44:36 -0300

    bootstrap webpack stuff

---
+++ src/MarketView/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MarketView = () => {
+  return (
+    <div>bitconneeeeeeeect</div>
+  )
+}
---
+++ src/root.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+import MarketView from './MarketView'
+
+
+const RootComponent = () => (
+  <MarketView />
+)
+
+export default hot(RootComponent)
