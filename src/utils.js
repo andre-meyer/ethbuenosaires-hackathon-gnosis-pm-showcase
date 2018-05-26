41cfbe6c802ba432d63f549f309c33b04d010a7b
Collin Chin <collin.chin@berkeley.edu>
2018-05-25 23:08:59 -0300

    rinkeby options

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const description = {
+export const description = {
     title: 'How many projects will be shipped out of ETHBuenosAires',
     description: 'A prediction market to determine the amount of completed projects that the 2018 ETHBuenosAires Hackathon will produce',
     resolutionDate: new Date("2018-05-27T10:00:00Z").toISOString(),
@@ -6,4 +6,10 @@ const description = {
     upperbound,
     decimals: 0,
     unit: "projects"
+}
+export const options = {
+    ethereum: "https://rinkeby.infura.io",
+    ipfs: '',
+    gnosisdb: 'https://db.gnosis.pm',
+    defaultAccount: '0x069fd4784D1DEd8A63923e83fF73c44414240043'
 }
